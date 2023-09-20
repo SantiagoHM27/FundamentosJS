70642e155f5a675c4c1513ab7de84a72f3bd3f3b
SantiagoHM27 <himejimadarkrai@gmail.com>
2023-09-20 12:41:35 -0600

    13 Objetos Javascript

---
+++ objetos.js
@@ -0,0 +1,42 @@
+// Objeto es una colección de datos
+// a travéz de key/value llave/valor
+
+/*
+objeto = {
+    key: 'valor',
+    'key': 'valor'
+}
+objeto.key
+objeto['key]
+*/
+
+const alumno = {
+    name: 'Steve',
+    age: 21,
+    city: 'Milan',
+    califications: [10, 10, 8],
+    career: 'TIADSM',
+    'last name': 'Jobs',
+    'gender': 'Male'
+}
+
+console.log(alumno);
+console.log(alumno.name);
+console.log(alumno.city);
+console.log(alumno['califications']);
+console.log(alumno['last name']);
+
+alumno.height = 1.75;
+console.log(alumno);
+
+const group = {
+    name: '4A DSM',
+    career: 'TIADSM',
+    classroom: 'Lab Redes 1',
+    subjects: ['Aplicaciones', 'Modelado', 'Data Bases'],
+    alumns: [alumno, alumno, alumno]
+}
+
+console.log(group);
+console.log(group.subjects[1]);
+console.log(group.alumns[0].califications[1]);
